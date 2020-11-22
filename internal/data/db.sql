4bf58ab35aab481cda34b22a0709b7f1c7202f6d
Marco Montesines <mamontesines@gmail.com>
2020-11-22 13:57:53 +0100

    add example json graph data upsert

--- internal/data/db.sql
+++ internal/data/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(254)  NOT NULL,
     apiKey CHAR(64) NOT NULL,
@@ -7,9 +7,21 @@ CREATE TABLE users(
     CONSTRAINT email_uniq UNIQUE (email)
 );
 
-CREATE INDEX idx_users_apiKey ON users(apiKey);
+CREATE INDEX IF NOT EXISTS idx_users_apiKey ON users(apiKey);
 
 
 INSERT INTO users(email, apiKey, created_at, updated_at) values('ask@me.com',
     '61ea4113f56ba6e8cdb313eeb4d953be6ba29796614046c5281d5e7b468a2765',
-    'NOW()', 'NOW()');
+    'NOW()', 'NOW()')
+ON CONFLICT DO NOTHING	;
+
+CREATE TABLE IF NOT EXISTS users_medical_data(
+    id INTEGER PRIMARY KEY NOT NULL,
+    data jsonb
+);
+
+CREATE INDEX IF NOT EXISTS idx_users_medical_data ON users_medical_data USING GIN (data jsonb_path_ops);
+
+INSERT INTO users_medical_data(id, data) values(1, jsonb_build_array(jsonb_build_object('ts',NOW(),'bodyTemparature',36.7,'heartRate',56)))
+ON CONFLICT (id) DO
+UPDATE SET id=excluded.id, data=users_medical_data.data||excluded.data;
